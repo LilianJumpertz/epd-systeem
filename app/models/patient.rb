cc456e443027ff76d5fcc2392c00d6141d9e50cb
Lilian <l.jumpertz@gmail.com>
2017-05-17 15:03:23 +0000

    change patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,7 @@ class Patient < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :gender, presence: true, , inclusion: { in: %w(man vrouw),
     message: "%{value} is not a valid size" }
-  # validates :BSN, presence: true, length: 8
+   validates :BSN, presence: true, length: 8
   # validates :date_of_birth, presence: true
   # validates :address, presence: true
   # validates :postal_code, presence: true
